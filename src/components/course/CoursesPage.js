9dcc2ea712602219f73511aa523ec1826059c0d9
Mark Eads <meads@hcpci.com>
2016-08-10 16:29:46 -0400

    mock api

--- src/components/course/CoursesPage.js
+++ src/components/course/CoursesPage.js
@@ -1,30 +1,12 @@
 import React, { Component, PropTypes } from 'react';
 import { connect } from 'react-redux';
 import * as courseActions from '../../actions/courseActions';
-import { bindActionCreators } from 'redux'
+import { bindActionCreators } from 'redux';
 
 class CoursesPage extends React.Component {
     constructor(props, context) {
         super(props,context);
-
-        this.state = {
-            course: { title: '' }
-        };
-
         //binds in the contructor are recommended 
-        this.onClickSave = this.onClickSave.bind(this);
-        this.onTitleChange = this.onTitleChange.bind(this);
-    }
-
-    onTitleChange(event) {
-        const course  = this.state.course;
-        course.title = event.target.value;
-        this.setState({ course: course});
-    }
-
-    onClickSave() {
-        //action is wrapped in call to dispatch down below
-       this.props.actions.createCourse(this.state.course);
     }
 
     courseRow(course, index) {
@@ -37,9 +19,6 @@ class CoursesPage extends React.Component {
             <div>
                 <h1>Courses</h1>
                 {this.props.courses.map(this.courseRow)}
-                <h2>Add Course</h2>
-                <input type="text" onChange={this.onTitleChange} value={this.state.course.title} />
-                <input type="submit" onClick={this.onClickSave} value="Save" />
             </div>
         );
     }
--- src/index.js
+++ src/index.js
@@ -7,8 +7,11 @@ import './styles/styles.css'; // imported from webpack
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import configureStore from './store/configureStore';
 import {Provider} from 'react-redux';
+import {loadCourses} from './actions/courseActions';
 
 const store = configureStore();
+
+store.dispatch(loadCourses());
 
 render (
     <Provider store={store}>
